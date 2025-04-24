0f1b8b6d181e33717bf1e980e1cd318f70481ec1
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-24 21:19:30 +0000

    commit

--- code.js
+++ code.js
@@ -5,8 +5,7 @@ const rows = [];
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-for (let i = 0; i < count; i = i + 1) {
-    rows.push(padRow(i + 1, count));
+for (let i = 0; i < count; i = i + 1) {rows.push(padRow(i + 1, count));
 }
 
 let result = ""
