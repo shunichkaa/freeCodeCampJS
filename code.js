31a6ffa939f5322370217adfbf09b2029d2ecb0d
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-25 20:49:06 +0200

    Update code.js
    Your loop needs to be updated to run when i is 8, too. Looking at your logic, this means your loop should run when i is less than or equal to count. You can use the less than or equal to operator <= for this.
    
    Update your loop condition to run while i is less than or equal to count.

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ const rows = [];
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-for (let i = 1; i < count; i++ ) {rows.push(padRow(i , count));
+for (let i = 1; i <= count; i++ ) {rows.push(padRow(i , count));
 }
 
 let result = ""
