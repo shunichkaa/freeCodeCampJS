6d1786f92acf96634a494f039d0768f557f0e1c2
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-25 20:50:08 +0200

    Update code.js
    Add a single-line comment above your for loop to remind yourself to change the code to a different kind of loop.

--- code.js
+++ code.js
@@ -5,6 +5,7 @@ const rows = [];
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
+//change the code to a different kind of loop
 for (let i = 1; i <= count; i++ ) {rows.push(padRow(i , count));
 }
 
