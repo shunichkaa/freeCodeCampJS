b4541b5c2471b8715c5dc93e81244ee64c6f2dba
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-27 15:22:09 +0200

    Start by creating a new for loop. Declare your iterator i and assign it the value of count, then use the boolean false for your condition and iteration statements.

--- code.js
+++ code.js
@@ -5,6 +5,8 @@ const rows = [];
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
+for (let i = count; false; false) {
+}
 //change the code to a different kind of loop
 for (let i = 1; i <= count; i++ ) {rows.push(padRow(i , count));
 }
