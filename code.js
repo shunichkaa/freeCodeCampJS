91499d56e4bef005d2f2f14dfc590c6f111313b9
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-29 11:57:06 +0200

    Update code.js

--- code.js
+++ code.js
@@ -9,8 +9,6 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-
 for (let i = 1; i <= count; i++) {
   if (inverted) {
     rows.unshift(padRow(i, count));
@@ -18,14 +16,6 @@ for (let i = 1; i <= count; i++) {
     rows.push(padRow(i, count));
   }
 }
-
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
-
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
 
 let result = ""
 
