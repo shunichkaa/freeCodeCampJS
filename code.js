d70ba924d3f674af0d3d51303e1d906855ce7931
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-29 12:04:15 +0200

    Update code.js

--- code.js
+++ code.js
@@ -4,21 +4,21 @@ const rows = [];
 let inverted = false;
 
 function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+	return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
 for (let i = 1; i <= count; i++) {
-  if (inverted) {
-    rows.unshift(padRow(i, count));
-  } else {
-    rows.push(padRow(i, count));
-  }
+	if (inverted) {
+		rows.unshift(padRow(i, count));
+	} else {
+		rows.push(padRow(i, count));
+	}
 }
 
 let result = ""
 
 for (const row of rows) {
-  result = result + row + "\n";
+	result = result + row + "\n";
 }
 
 console.log(result);
